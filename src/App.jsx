15318e6d2ca71f585540899c9d627ad9facb7a6b
jeremy viterbo <webd0829@gmail.com>
2026-04-08 07:37:36 +0800

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page not found.</p>
+    </div>
+  )
+}
